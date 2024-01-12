7b56148c60f66e77fb16001d07cf64d2eab72339
Nathaniel Escaro <butterman423@gmail.com>
2024-01-12 01:16:51 -0500

    Removed Leaf Event Listener, Reset to baseplate

--- scripts/main.ts
+++ scripts/main.ts
@@ -2,8 +2,6 @@ import type { View, WorkspaceLeaf } from 'obsidian'
 import { Plugin } from 'obsidian';
 
 export default class LatexViewerPlugin extends Plugin {
-    target: HTMLElement | null
-
     async onload() {
         const workspace = this.app.workspace;
 
@@ -13,33 +11,9 @@ export default class LatexViewerPlugin extends Plugin {
         });
 
         // Commands in separate file (if any)
-
-        this.registerEvent(workspace.on('active-leaf-change', (leaf: WorkspaceLeaf) => {
-            if(leaf == null) { return; }
-            this.setTargetView(leaf.view);
-        }))
     }
 
     async onunload() {
-        if(this.target != null) {
-            this.target.removeEventListener('click', this.clickCallback);
-        }
-    }
-
-    setTargetView(view: View) {
-        if(this.target != null) {
-            this.target.removeEventListener('click', this.clickCallback);
-        }
-        if(view.getViewType() != 'markdown') {
-            this.target = null;
-            return;
-        }
-
-        this.target = view.containerEl;
-        view.registerDomEvent(view.containerEl, 'click', this.clickCallback);
-    }
-
-    clickCallback(evt: MouseEvent) {
-        console.log("I have been clicked!");
+        
     }
 }
