bbd7b83cde6227f9eff34d95e5a5bc8730c3060f
Nathaniel Escaro <butterman423@gmail.com>
2024-01-13 15:55:28 -0500

    Added editor and click listeners on active file

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,8 +1,12 @@
-import type { View, WorkspaceLeaf } from 'obsidian'
-import { Plugin } from 'obsidian';
+import type { Editor, TFile } from 'obsidian'
+import { Plugin, MarkdownView} from 'obsidian';
 
 export default class LatexViewerPlugin extends Plugin {
+    clickRegistered: boolean
+
     async onload() {
+        this.clickRegistered = false
+
         const workspace = this.app.workspace;
 
         // Load in ribbon icon
@@ -11,9 +15,45 @@ export default class LatexViewerPlugin extends Plugin {
         });
 
         // Commands in separate file (if any)
+
+        // Listen to user edits
+        this.registerEvent( workspace.on('editor-change', this.onEditorChange) );
+
+        // File to register click listener
+        this.registerEvent( workspace.on('file-open', (file: TFile | null) => {
+            if(file == null) { 
+                this.clickRegistered = false;
+                return;
+            }
+
+            if(this.clickRegistered) { return }
+            if( (file as TFile).extension != 'md' ) { return }
+
+            console.log('Registering click event')
+
+            this.clickRegistered = true
+            this.registerClickEvent(workspace.containerEl)
+        }) );
     }
 
     async onunload() {
         
+    }
+
+    onEditorChange(editor: Editor, info: MarkdownView) {
+        //console.log(info.getViewData())
+        //console.log(editor.getCursor())
+        console.log('Changed!');
+    }
+
+    registerClickEvent(workEl: HTMLElement) {
+        const clickEl: HTMLElement | null = workEl.querySelector<HTMLElement>('div.cm-content')
+
+        if( clickEl == null ) {
+            // Something has gone horribly wrong, abort
+            return
+        }
+
+        this.registerDomEvent( clickEl as HTMLElement, 'click', (ev: MouseEvent) => {} )
     }
 }
