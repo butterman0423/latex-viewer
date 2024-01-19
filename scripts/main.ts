d1aa6949c7f0ba632293171a91a6804ecef0e60c
Nathaniel Escaro <butterman423@gmail.com>
2024-01-19 16:21:53 -0500

    Click event now calls editor-change

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,4 +1,4 @@
-import type { TFile } from 'obsidian'
+import type { MarkdownFileInfo, TFile } from 'obsidian'
 import type { PluginSettings } from './settings';
 
 import { MarkdownRenderer, Plugin } from 'obsidian';
@@ -8,69 +8,49 @@ import { LatexView, VIEW_TYPE } from './ext-view';
 export default class LatexViewerPlugin extends Plugin {
     settings: PluginSettings
 
-    clickRegistered: boolean
     mdContent: HTMLElement | null
-
     latexSpace: HTMLElement
 
     async onload() {
         await loadSettings(this);
         const workspace = this.app.workspace;
 
-        this.clickRegistered = false;
-        this.latexSpace = this.createLatexSpace(workspace.containerEl);
-
         // Commands in separate file (if any)
 
         // Registering view
         this.registerView(VIEW_TYPE, (leaf) => new LatexView(leaf));
-
-        // Listen to user edits
-        //this.registerEvent( workspace.on('editor-change', this.updateView) );
-
-        // File to register click listener
-        this.registerEvent( workspace.on('file-open', (file: TFile | null) => {
-            if(file == null) { 
-                this.clickRegistered = false;
-                return;
-            }
-
-            if(this.clickRegistered) { return }
-            if( (file as TFile).extension != 'md' ) { return }
-
-            this.clickRegistered = true;
-            this.registerClickEvent(workspace.containerEl);
-        }) );
-
+        
         workspace.onLayoutReady(async () => {
             // Show leaf
             const leaf = workspace.getRightLeaf(false);
             await leaf.setViewState({ type: VIEW_TYPE, active: true });
+
+            this.mdContent = workspace.containerEl.querySelector<HTMLElement>('div.cm-content');
+            this.latexSpace = this.createLatexSpace(workspace.containerEl);
+
+            // Listen to user edits
+            this.registerEvent( workspace.on('editor-change', async (editor, _info) => {
+                if(editor != undefined && editor.hasFocus()) {
+                    
+                }
+            }) );
+
+            // Listen to click events
+            this.registerDomEvent( workspace.containerEl, 'click', async () => {
+                const { activeEditor } = workspace;
+                workspace.trigger('editor-change', activeEditor?.editor, activeEditor);
+            })
         });
+
+        
     }
 
     async onunload() {
         LatexView.destroyAll(this.app.workspace);
     }
 
-    registerClickEvent(workEl: HTMLElement) {
-        const clickEl: HTMLElement | null = workEl.querySelector<HTMLElement>('div.cm-content')
-
-        if( clickEl == null ) {
-            // Something has gone wrong, abort
-            return
-        }
-
-        this.mdContent = clickEl;
-        this.registerDomEvent( this.mdContent as HTMLElement, 'click', this.updateView );
-
-        console.log('Registering click event');
-    }
-
     async updateView() {
-        console.log('Updating');
-        // TODO: There seems to be a bug where null is not being caught
-        if(this.mdContent == null || this.mdContent == undefined) { return }
+        if(this.mdContent == null) { return }
 
         const latexBlock: NodeList = this.mdContent.querySelectorAll('span.cm-math:not(.cm-formatting-math)');
         let code: string = "";
